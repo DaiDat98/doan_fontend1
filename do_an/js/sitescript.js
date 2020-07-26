51faeba7b5fba5d15eedea49b6faaeca46961f9f
boy nhok <caodaidat.tdc2018@gmail.com>
2020-07-27 00:01:19 +0700

    first commit

---
+++ do_an/js/sitescript.js
@@ -0,0 +1,12 @@
+// const toggleMenu = document.getElementById('toggle-menu');
+// const asideMenu = document.getElementById('aside-menu');
+// var isToggle = false;
+// toggleMenu.addEventListener('click', function(){
+//     if(isToggle){
+//         asideMenu.style.transform = "translateX(0)";
+//         isToggle = false;
+//     }else{
+//         asideMenu.style.transform = "translateX(-250px)";
+//         isToggle = true;
+//     }
+// });
